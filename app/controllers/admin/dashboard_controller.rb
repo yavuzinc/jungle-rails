8be4a4d0b6d80befaab2f47ee8d4eefec44ef20d
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-11-10 13:32:44 -0500

    added count and basic http auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+    # Handles basic HTTP Auth
+    http_basic_authenticate_with :name => "Jungle", :password => "book" 
   def show
+    @product_count = Product.count
+
+    @category_list = Product.group(:category_id).count
   end
 end
