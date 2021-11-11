00537be41326164b87ebdb7b606e371c5e8c6ee8
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-11-11 14:43:38 -0500

    added User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,15 @@ class ApplicationController < ActionController::Base
     enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
   end
   helper_method :cart_subtotal_cents
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 
 
   def update_cart(new_cart)
